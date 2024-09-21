7fa2bb74eb04a02299d026e5f370a7577166e885
todorokiworks2 <todorokiworks@gmail.com>
2024-09-21 13:47:30 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 import playformCompress from '@playform/compress';
-import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
